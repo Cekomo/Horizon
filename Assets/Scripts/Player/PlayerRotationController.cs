743fac9e081293f50c194a8599b0f9ad2f7719ba
cekomo <cemil.sahin@ozu.edu.tr>
2023-04-18 04:14:48 +0300

    change rotation control system

--- Assets/Scripts/Player/PlayerRotationController.cs
+++ Assets/Scripts/Player/PlayerRotationController.cs
@@ -4,19 +4,43 @@ namespace Player
 {
     public class PlayerRotationController : MonoBehaviour
     {
-        [Range(1f, 10f)] [SerializeField] private float rotationSpeed = 1f;
+        // [Range(1f, 10f)] [SerializeField] private float rotationSpeed = 1f;
+        
+        private bool isRotationLeft;
+        private bool isDirectionChangeable;
         
         private void Update()
         {
-            if (!Input.GetMouseButton(1)) return;
-            
+            GetPlayerRotation();
+            if (!Input.GetKey(KeyCode.W)) return;
             RotatePlayerHorizontally();
         }
 
+        private void GetPlayerRotation()
+        {
+            if (!(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))) return;
+                
+            isDirectionChangeable = true;
+            
+            if (Input.GetKey(KeyCode.D))
+                isRotationLeft = false;
+            else if (Input.GetKey(KeyCode.A))
+                isRotationLeft = true;
+        }
+        
         private void RotatePlayerHorizontally()
         {
-            var horizontalRotation = Input.GetAxis("Mouse X") * rotationSpeed;
-            transform.Rotate(Vector3.up, horizontalRotation);
+            if (!isDirectionChangeable) return;
+
+            if (isRotationLeft)
+                transform.Rotate(Vector3.up, -90.0f, Space.Self);
+            else
+                transform.Rotate(Vector3.up, 90.0f, Space.Self);
+
+            isDirectionChangeable = false;
+
+            // var horizontalRotation = Input.GetAxis("Mouse X") * rotationSpeed;
+            // transform.Rotate(Vector3.up, horizontalRotation);
         }
     }
 }
